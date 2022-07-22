fef8ccea28588eb53a2351646cf2c22d45bc7e70
Kobir Chowdhury <kobir.chowdhury@yahoo.com>
2022-07-21 20:07:25 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-one
+# myWebGameProjectOne
+I am developing a web based game. I am using HTML, CSS and JavaScripts.
---
+++ script.js
@@ -0,0 +1,18 @@
+// Update Loop
+import Ball from " ./Ball.js"
+const ball = new Ball(document.getElementById("ball"))
+
+    let lastTime 
+    function update(time) {
+        if(lastTime != null) {
+            const delta = time-lastTime
+            //Update code.
+            ball.update(delta)
+        }
+
+        lastTime = time
+        window.requestAnimationFrame(update)
+    }
+
+
+window.requestAnimationFrame(update)
